fc628a82dfb229be195c8150957097b587620706
Anže Sodja <asodja@gradle.com>
2022-05-31 12:54:14 +0200

    Remove accidentally added checkstyle from the classpath

--- projects/app/build.gradle.kts
+++ projects/app/build.gradle.kts
@@ -5,8 +5,6 @@ plugins {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("com.puppycrawl.tools:checkstyle:10.2")
-
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
 }
 
