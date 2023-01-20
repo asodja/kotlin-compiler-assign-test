e031f997143c3a2c36cac194b56b0750b2b702ff
Anže Sodja <asodja@gradle.com>
2023-01-20 11:14:04 +0100

    Remove some cases

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,9 +28,3 @@ tasks.withType<KotlinCompile> {
     compilerOptions.noJdk.set(false) // Go to declaration works
     compilerOptions.noJdk = false // Go to declaration works
 }
-
-java.docsDir.set(file("src/docs")) // no error
-java.docsDir = file("src/docs") // an error, but should not be
-java {
-    docsDir = file("src/docs") // no error
-}
